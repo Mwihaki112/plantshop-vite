ac48e8614800fe733a3bec1d330703ffe9355d00
Agnes Mwihaki <moringa@DESKTOP-89EPAHO.localdomain>
2026-02-22 13:41:48 +0300

    Initial commit

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // Fetch plants on page load
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // Add new plant (POST request)
+  function handleAddPlant(newPlant) {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((res) => res.json())
+      .then((createdPlant) => {
+        setPlants([...plants, createdPlant]);
+      });
+  }
+
+  // Toggle sold out (non-persisting)
+  function handleToggleStock(id) {
+    const updatedPlants = plants.map((plant) =>
+      plant.id === id
+        ? { ...plant, inStock: !plant.inStock }
+        : plant
+    );
+    setPlants(updatedPlants);
+  }
+
+  // Filter plants
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
